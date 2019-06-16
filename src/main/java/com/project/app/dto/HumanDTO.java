3189ada9e4e8d6cb80a78c3bce375b6da70ee347
Nahid Asadioun <nahidasadioun@gmail.com>
2019-06-16 23:57:34 +0430

    HumanDto.java: Class DTO for relation with Controller

---
+++ src/main/java/com/project/app/dto/HumanDTO.java
@@ -0,0 +1,73 @@
+package com.project.app.dto;
+
+public class HumanDTO{
+
+    private int id ;
+    private int age;
+    private String sex;
+    private double salary;
+    private String type;
+
+    public HumanDTO(int id, int age, String sex, double salary, String type) {
+        this.id = id;
+        this.age = age;
+        this.sex = sex;
+        this.salary = salary;
+        this.type = type;
+    }
+
+    public HumanDTO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "id=" + id +
+                ", age=" + age +
+                ", sex='" + sex + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+}
