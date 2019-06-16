964ec647b36764b2e720f02fd7efe31d9e44a4ad
Nahid Asadioun <nahidasadioun@gmail.com>
2019-06-16 23:50:29 +0430

    Repository.java: interface class and another class impiliment this interface

--- src/main/java/com/project/app/reposiory/Repository.java
+++ src/main/java/com/project/app/reposiory/Repository.java
@@ -1,4 +1,10 @@
 package com.project.app.reposiory;
 
+import com.project.app.model.Human;
+
 public interface Repository {
+
+        Human read(int id);
+        void write(Human human);
+        void printall();
 }
