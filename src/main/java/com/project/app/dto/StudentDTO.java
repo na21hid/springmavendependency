f50c1de7318ec7df1a00f8ad073c5e9f7caff99e
Nahid Asadioun <nahidasadioun@gmail.com>
2019-06-16 23:58:50 +0430

    StudentDTO.java: class for relation with student controller

---
+++ src/main/java/com/project/app/dto/StudentDTO.java
@@ -0,0 +1,8 @@
+package com.project.app.dto;
+
+public class StudentDTO extends HumanDTO {
+
+    public StudentDTO(int id, int age, String sex, double salary, String type) {
+        super(id, age, sex, salary, type);
+    }
+}
