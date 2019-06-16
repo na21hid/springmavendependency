a2367944e453eb9f0c11c441bfa1b75d40f157a5
Nahid Asadioun <nahidasadioun@gmail.com>
2019-06-16 23:41:53 +0430

    ProgrammerDTO.java: Class for use in controller class

--- src/main/java/com/project/app/dto/ProgramerDTO.java
+++ src/main/java/com/project/app/dto/ProgramerDTO.java
@@ -1,17 +1,11 @@
 package com.project.app.dto;
 
-import com.project.app.model.Human;
+public class ProgramerDTO extends HumanDTO {
 
-public class Programer extends Human {
-
-    public Programer(int id, int age, String sex, double salary,String type) {
+    public ProgramerDTO(int id, int age, String sex, double salary, String type) {
         super(id, age, sex, salary, type);
     }
 
 
-    @Override
-    public String toString() {
-        return super.toString();
-    }
 
 }
