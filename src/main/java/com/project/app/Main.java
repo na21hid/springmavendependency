a98595e5a60a638c6bfe75bc4d1abb52dd5234ee
Nahid Asadioun <nahidasadioun@gmail.com>
2019-06-21 18:31:48 +0430

    main.java: change package

--- src/main/java/com/project/app/Main.java
+++ src/main/java/com/project/app/Main.java
@@ -1,14 +1,15 @@
-package main;
+package com.project.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
-@ComponentScan(basePackages = "com.project.app.*")
+@ComponentScan(basePackages = "com.project.*")
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
 }
+//#bebin too ComponentScan gofti az com..project begard vali khode classe mainet ro borde boodi biroon e com.*
