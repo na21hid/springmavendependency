58ce6b92a104ccca40d4b5916720db8e373f5090
Nahid Asadioun <nahidasadioun@gmail.com>
2019-06-21 18:37:27 +0430

    programmerDto: add class dto

--- src/main/java/com/project/app/dto/ProgramerDTO.java
+++ src/main/java/com/project/app/dto/ProgramerDTO.java
@@ -2,7 +2,7 @@ package com.project.app.dto;
 
 public class ProgramerDTO extends HumanDTO {
 
-    public ProgramerDTO(int id, int age, String sex, double salary, String type) {
+    public ProgramerDTO(Long id, int age, String sex, double salary, String type) {
         super(id, age, sex, salary, type);
     }
 
